76eff7adc39984b7f7a735b7af473feec511bf9a
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2019-02-20 20:18:09 +0300

    Basic UI test for post detail page

--- PostBrowserUITests/PostBrowserUITests.swift
+++ PostBrowserUITests/PostBrowserUITests.swift
@@ -26,9 +26,13 @@ class PostBrowserUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testIfPostDetailViewControllerHasData() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        app.staticTexts["titleLabel"].firstMatch.tap()
+        XCTAssertNotNil(app.staticTexts["detailTitleLabel"].value)
+        XCTAssertNotNil(app.textViews["detailBodyTextView"].value)
     }
 
 }
